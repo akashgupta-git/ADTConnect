3e97a34fcd1030d25f1002b6feab11a9c54c4342
Akash Gupta <akashgupta.20044@gmail.com>
2024-11-05 01:54:41 +0530

    Deployment Commit 2

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -89,7 +89,10 @@ const MeetingRoom = () => {
           </DropdownMenuContent>
         </DropdownMenu>
         <CallStatsButton />
-        <button onClick={() => setShowParticipants((prev) => !prev)}>
+        <button
+          onClick={() => setShowParticipants((prev) => !prev)}
+          aria-label="Toggle Participants List"
+        >
           <div className=" cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]  ">
             <Users size={20} className="text-white" />
           </div>
